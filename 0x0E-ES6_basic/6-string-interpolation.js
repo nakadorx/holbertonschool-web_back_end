b100ee9cd3b5ff346127c40ef5741f580c7c92b0
nakadorx <nakadorx@gmail.com>
2021-05-25 18:19:28 +0200

    all

--- 0x0E-ES6_basic/6-string-interpolation.js
+++ 0x0E-ES6_basic/6-string-interpolation.js
@@ -1,9 +1,9 @@
 export default function getSanFranciscoDescription() {
   const year = 2017;
   const budget = {
-    income: "$119,868",
-    gdp: "$154.2 billion",
-    capita: "$178,479",
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
   };
 
   return (
