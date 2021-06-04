1c1065b6215ae4c4289d4f6f707659341ae6ff61
nakadorx <nakadorx@gmail.com>
2021-06-04 17:02:36 +0200

    all

--- 0x0F-ES6_promise/1-promise.js
+++ 0x0F-ES6_promise/1-promise.js
@@ -1,13 +1,9 @@
 export default function getFullResponseFromAPI(success) {
-  const res = new Promise((resolve, reject) => {
-    if (success === true) {
-      resolve({
-        status: 200,
-        body: "Success",
-      });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: "Success" });
     } else {
       reject(new Error("The fake API is not working currently"));
     }
   });
-  return res;
 }
