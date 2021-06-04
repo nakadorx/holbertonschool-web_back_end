7b3db4972837825e6b0d2f8bd5138ce83b2ca4c1
nakadorx <nakadorx@gmail.com>
2021-06-04 17:06:55 +0200

    all

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
+  console.log("Got a response from the API");
   return promise
     .then(() => ({
       status: 200,
       body: "success",
     }))
-    .catch(() => new Error())
-    .finally(() => {
-      console.log("Got a response from the API");
-    });
+    .catch(() => new Error());
 }
