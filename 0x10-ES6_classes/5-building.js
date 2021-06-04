52e6802f11d35d027bdef8d77acf9e67042ebda2
nakadorx <nakadorx@gmail.com>
2021-06-04 17:59:14 +0200

    all

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,16 +1,14 @@
 export default class Building {
-    constructor(sqft) {
-      if (this.constructor !== Building) {
-        if (!this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
-      }
-      this._sqft = sqft;
-    }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    set sqft(sqft) {
-      this._sqft = sqft;
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage'
+      );
     }
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
