39e990f3f8f9eb59f58da8786843262f5a26cd02
nakadorx <nakadorx@gmail.com>
2021-06-04 17:54:45 +0200

    all

--- 0x10-ES6_classes/0-classroom.js
+++ 0x10-ES6_classes/0-classroom.js
@@ -1,9 +1,5 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
-    }
-  }import ClassRoom from './0-classroom';
-
-  export default function initializeRooms() {
-    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
   }
+}
