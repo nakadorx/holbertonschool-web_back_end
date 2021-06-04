45647dd5457eaef376e7653ff4e6019abb811e15
nakadorx <nakadorx@gmail.com>
2021-06-04 17:44:07 +0200

    all

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((res) => {
-      const { body } = res[0];
-      const { firstName, lastName } = res[1];
-      console.log(`${body} ${firstName} ${lastName}`);
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => console.log('Signup system offline'));
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
