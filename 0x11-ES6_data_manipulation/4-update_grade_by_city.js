7e922b2445bf733074eaece852580a60ada01424
nakadorx <nakadorx@gmail.com>
2021-06-05 02:06:13 +0200

    all

--- 0x11-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x11-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,11 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr;
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (Object.getPrototypeOf(students, newGrades) !== Array.prototype) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((std) => {
+      const [newGrade] = newGrades.filter((item) => item.studentId === std.id);
+      return { ...std, grade: newGrade ? newGrade.grade : 'N/A' };
+    });
 }
