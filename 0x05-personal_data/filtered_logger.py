94cae2ce3aa4cd72fc5907bd0148694054a9e60f
nakadorx <nakadorx@gmail.com>
2021-06-13 00:17:50 +0200

    all

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -1,35 +1,49 @@
 #!/usr/bin/env python3
-"""
-holb
+"""holb
 """
 
+from typing import List
 import re
 import logging
-import os
-from typing import List
 import mysql.connector
+import os
 
-
-PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 class RedactingFormatter(logging.Formatter):
-    """ holb """
+    """ Redacting Formatter class
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        self.fields = fields
+    def __init__(self, fields: List[str]):
+        """holb
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """holb
         """
-        holb
-        """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+        return filter_datum(self.fields,
+                            self.REDACTION,
+                            super().format(record),
+                            self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """holb
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
 
 
 def filter_datum(fields: List[str],
@@ -39,66 +53,28 @@ def filter_datum(fields: List[str],
     """holb
     """
     for field in fields:
-        message = re.sub(rf"{field}=.*?{separator}",
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub('{}=.*?{}'.format(field, separator),
+                         '{}={}{}'.format(field, redaction, separator),
+                         message)
     return message
-
-
-def get_logger() -> logging.Logger:
-    """holb
-    """
-
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    logStreamHandler = logging.StreamHandler()
-    logStreamHandler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(logStreamHandler)
-
-    return logger
 
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """holb
     """
+    db_user = os.environ.get('PERSONAL_DATA_DB_USERNAME', None)
+    db_pw = os.environ.get('PERSONAL_DATA_DB_PASSWORD', None)
+    db_host = os.environ.get('PERSONAL_DATA_DB_HOST', None)
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME', None)
 
-    dbName = os.environ.get("PERSONAL_DATA_DB_NAME")
-    dbUserName = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
-    dbUserPassword = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
-    dbHost = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
-
-    dbConnector = mysql.connector.connect(user=dbUserName,
-                                          password=dbUserPassword,
-                                          host=dbHost,
-                                          database=dbName)
-    return dbConnector
+    return mysql.connector.connect(user=db_user,
+                                   password=db_pw,
+                                   host=db_host,
+                                   database=db_name)
 
 
 def main():
-    """holb
-    """
-
-    mySQLconnector = get_db()
-    cursor = mySQLconnector.cursor()
-    tableName = "users"
-    query = ("SELECT * FROM %s")
-    cursor.execute(query, tableName)
-
-    sqlLogger = get_logger()
-
-    retrievedData = []
-    for row in cursor:
-        inf = f"name={row[0]}; email={row[1]}; phone={row[2]}; " \
-              f"ssn={row[3]}; password={row[4]}; ip={row[5]}; " \
-              f"last_login={row[6]}; user_agent={row[7]};"
-        retrievedData.append(inf)
-
-    for data in retrievedData:
-        sqlLogger.info(data)
-
-    cursor.close()
-    mySQLconnector.close()
-
+    pass
 
 if __name__ == "__main__":
     main()
